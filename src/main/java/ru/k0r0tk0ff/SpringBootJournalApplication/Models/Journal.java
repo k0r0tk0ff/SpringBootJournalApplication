d348c8d2e6794605fd2b8335e740ec65d31a6d49
Alexandr <korotkov34reg@gmail.com>
2017-09-28 09:42:34 +0300

    Update Journal.java

--- src/main/java/ru/k0r0tk0ff/SpringBootJournalApplication/Models/Journal.java
+++ src/main/java/ru/k0r0tk0ff/SpringBootJournalApplication/Models/Journal.java
@@ -1,4 +1,4 @@
-package ru.k0r0tk0ff.SpringBootJournalApplication.Models;
+ackage ru.k0r0tk0ff.SpringBootJournalApplication.Models;
 
 import javax.persistence.*;
 
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-/**
+/**.
  * Created by k0r0tk0ff on 9/27/2017.
  */
 
@@ -23,89 +23,51 @@ public class Journal {
     private Date created;
     private String summary;
 
+    /*
+    * @Transient означает, что переменая SimpleDateFormat format не будет записываться в базу данных
+    */
     @Transient
     private SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
 
+    /*
+    * Для корректной работы Spring Data JPA необходимы 
+    * a) коструктор со всеми параметрами 
+    */   
     public Journal(String title, String summary, String date) throws ParseException {
         this.title = title;
         this.summary = summary;
         this.created = format.parse(date);
     }
 
+    /*
+    * б) коструктор без параметров 
+    */
     public Journal(){}
 
-    /**
-     * Getter for property 'id'.
-     *
-     * @return Value for property 'id'.
-     */
     public long getId() {
         return id;
     }
-
-    /**
-     * Setter for property 'id'.
-     *
-     * @param id Value to set for property 'id'.
-     */
     public void setId(long id) {
         this.id = id;
     }
-
-    /**
-     * Getter for property 'title'.
-     *
-     * @return Value for property 'title'.
-     */
     public String getTitle() {
         return title;
     }
-
-    /**
-     * Setter for property 'title'.
-     *
-     * @param title Value to set for property 'title'.
-     */
     public void setTitle(String title) {
         this.title = title;
     }
-
-    /**
-     * Getter for property 'created'.
-     *
-     * @return Value for property 'created'.
-     */
     public Date getCreated() {
         return created;
     }
-
-    /**
-     * Setter for property 'created'.
-     *
-     * @param created Value to set for property 'created'.
-     */
     public void setCreated(Date created) {
         this.created = created;
     }
-
-    /**
-     * Getter for property 'summary'.
-     *
-     * @return Value for property 'summary'.
-     */
     public String getSummary() {
         return summary;
     }
-
-    /**
-     * Setter for property 'summary'.
-     *
-     * @param summary Value to set for property 'summary'.
-     */
     public void setSummary(String summary) {
         this.summary = summary;
     }
-
     public String getCreatedAsShort() {
         return format.format(created);
     }
