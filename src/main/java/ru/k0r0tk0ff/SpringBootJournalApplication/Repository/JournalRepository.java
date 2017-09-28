628c213f17bfbbd0036dcdc9566a032ce4a2d969
Alexandr <korotkov34reg@gmail.com>
2017-09-28 09:32:05 +0300

    Update JournalRepository.java

--- src/main/java/ru/k0r0tk0ff/SpringBootJournalApplication/Repository/JournalRepository.java
+++ src/main/java/ru/k0r0tk0ff/SpringBootJournalApplication/Repository/JournalRepository.java
@@ -4,7 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.k0r0tk0ff.SpringBootJournalApplication.Models.Journal;
 
 /**
- * Created by user on 9/27/2017.
+ * Created by k0r0tk0ff on 9/27/2017.
+ *
+ * JpaRepository — это интерфейс фреймворка Spring Data предоставляющий набор стандартных методов JPA для работы с БД.
+ * например List<Object> getAll() - возвратит список 
+ *
+ * можно написать свой кастомный метод -
+ * public interface BankRepository extends JpaRepository<Journal, Long> {
+ *
+ *   @Query("select b from Journal b where b.title = :titleName")
+ *   Journal findByTitle(@Param("titleName") String titleName);
+ * 
+ *}
+ * 
  */
 public interface JournalRepository extends JpaRepository <Journal, Long> {
 }
